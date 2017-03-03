c205f3003a3186e9f65053ee42127b22f9cffac8
Russell Schmidt <russell.schmidt@me.com>
2017-03-03 15:07:36 -0800

    add bootstrap.css and fonts/ directory. Had to adjust bootstrap.min.css for new file location inside App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './Bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="divider">
-        <h2>Questions</h2><hr/>
-      </div>
+      <Divider></Divider>
     );
   }
 }
+
+var Divider = React.createClass({
+  render: function() {
+    return (
+    <div className="divider">
+      <h2>Questions</h2><hr/>
+    </div>
+    );
+  }
+});
 
 export default App;
